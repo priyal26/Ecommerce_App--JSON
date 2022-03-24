7b5cb6308a175a384619ec0571207af509a88fa4
studiomac-154 <studiomac-154@studiomac-154s-iMac.local>
2022-03-23 21:31:15 -0700

    sequlize

--- app.js
+++ app.js
@@ -3,22 +3,64 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const errorController = require('./controllers/error');
+const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
+
 const app = express();
 
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
-app.use(shopRoutes);
-
-app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page Not Found'});
+app.use((req,res,next)=>{
+  User.findById(1)
+  .then(user => {
+    req.user = user;
+    next();
+  }).catch(err => console.log(err));
 });
 
-app.listen(3000);
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+
+app.use(errorController.get404);
+
+Product.belongsTo(User,{constraints :true,onDelete : 'CASCADE'});
+User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through : CartItem});
+Product.belongsToMany(Cart, {through : CartItem});
+
+sequelize
+  //.sync({force : true })
+  .sync()
+  .then(result => {
+    return User.findById(1);
+   
+  })
+  .then(user=>{
+    if(!user){
+      User.create({name:'Priyal',email:'pilssolanki111@gmail.com'});
+    }
+    return user;
+  })
+  .then(user => {
+   // console.log(user); 
+   return user.createCart();
+  }).then(cart =>{
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });    
--- first.js
+++ first.js
@@ -1,14 +1,6 @@
-console.log('Hello from node.JS');
+const http = require('http');
 
-var name = 'Priyal';
-var age = 21;
-var hobby = true;
-
-function summary(Uname, Uage, Uhobby){
-    return ('Name is' + Uname + ', the age is' + Uage + ', is there any hobby? ' + Uhobby
-    );
-}
-
-console.log(summary(name,age,hobby));
-
-console.log('the name is nefjgjgjg');
+const server = http.createServer((req,res)=>{
+    console.log(req);
+})
+server.listen(1000)
