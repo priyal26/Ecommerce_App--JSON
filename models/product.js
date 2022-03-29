7888e4bdfcf010e0515e2830eadbdeb054e28ba1
studiomac-154 <studiomac-154@studiomac-154s-iMac.local>
2022-03-29 00:40:47 -0700

    mongo

--- models/product.js
+++ models/product.js
@@ -1,3 +1,5 @@
+const { get } = require('express/lib/response');
+
 const getDb = require('../util/database').getDb;
 
 class Product {
@@ -35,6 +37,17 @@ class Product {
         console.log(err);
       });
   }
+
+  static deleteById(prodId)
+  {
+    const db = getDb();
+    db.collection('producrts')
+  }
+
+  static findById(prodId){
+    const db = getDb();
+  }
 }
+
 
 module.exports = Product;
